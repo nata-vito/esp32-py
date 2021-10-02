9b977de1e7b5c7860f1c5152f1b7e2850b71cc8b
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-10-02 20:55:13 -0300

    Toggle Pin added

--- togglePin.py
+++ togglePin.py
@@ -1,6 +1,12 @@
-import machine
-import time
-from machine import Pin
+import machine                                      # Main Lib to work with ESP in micropython
+import time                                         # Lib to delay
+from machine import Pin                             # For select pins
+
+led = machine.Pin(2, machine.Pin.OUT)               # Standard onboard led pin
 
 def toggle(p):
     p.value(not p.value())
+
+while True:
+    toggle(led)
+    time.sleep(1)
