2eae1f0f81866b025e184ce72f7776d5c8b9c5d3
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-10-02 21:43:12 -0300

    More readable

--- wifiConfig.py
+++ wifiConfig.py
@@ -3,21 +3,14 @@ import network
 sta_if = network.WLAN(network.STA_IF)               # Instance to station, please read the documentation for more details
 ap_if = network.WLAN(network.AP_IF)                 # Instance to acess point, ..........................................
 
-print("STA_IF: ")                                 
-print(sta_if.active())                                                # Cheking if station mode is active
-print("AP_IF: ")               
-print(ap_if.active())                                                 # Cheking if acess point is active
-print("AP Settings: ")                                                
-print(ap_if.ifconfig())                                               # Cheking wifi settings from acess point
-print("STA Settings: ")                                                
-print(sta_if.ifconfig())                                              # Cheking wifi settings from station 
+print("STA_IF: ", sta_if.active())                                   # Cheking if station mode is active
+print("AP_IF: ", ap_if.active())                                     # Cheking if acess point is active
+print("AP Settings: ", ap_if.ifconfig())                             # Cheking wifi settings from acess point
+print("STA Settings: ", sta_if.ifconfig())                           # Cheking wifi settings from station 
 
-sta_if.active(True)                                                   # Activating the station mode 
-print("STA_IF: ")                                 
-print(sta_if.active())                                                # Cheking if station mode is active
+sta_if.active(True)                                                  # Activating the station mode 
+print("STA_IF: ", sta_if.active())                                   # Cheking if station mode is active
 
 sta_if.connect('<your ESSID>', '<your password>')                    # Conect to your wifi router
-print("STA Is Connected: ")                    
-print(sta_if.isconnected())                                           # Cheking connection
-print("STA Settings: ")                           
-print(sta_if.ifconfig())                                              # Cheking wifi settings from station 
+print("STA Is Connected: ", sta_if.isconnected())                    # Cheking connection
+print("STA Settings: ", sta_if.ifconfig())                           # Cheking wifi settings from station 
